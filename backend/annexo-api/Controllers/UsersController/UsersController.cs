9e694d3cbb806ad735cddbff8b2b2aad5b85e8e1
annecarmen <annecarmenrubio@gmail.com>
2026-07-23 19:07:19 -0300

    feat add new route users

---
+++ backend/annexo-api/Controllers/UsersController/UsersController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace annexo_api.Controllers.UsersController;
+
+[ApiController]
+[Route("users")]
+
+public class UsersController : ControllerBase
+{
+    [HttpGet]
+    public async Task<IActionResult> GetUsers()
+    {
+        // Logic to retrieve users from the database or any other source
+        var users = new List<string> { "user1", "user2", "user3" };
+        return Ok(users);
+    }
+
+}
